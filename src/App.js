c7a057c1670f1bb795aeb6a34c732c88e54eff06
janjarozek <74878517+janjarozek@users.noreply.github.com>
2021-12-01 22:31:05 +0100

    updated routing with react-router-dom v6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Layout from './components/layout/Layout';
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Settings from './pages/Settings'
 
 function App() {
   return (
-    <div className="App">
-      <Layout />
-    </div>
+    <Router>
+      <div className="App">
+        <Layout>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route exact path="/settings" element={<Settings />} />
+          </Routes>
+        </Layout>
+      </div>
+    </Router>
   );
 }
 
